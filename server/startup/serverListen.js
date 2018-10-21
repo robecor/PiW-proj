97179eccc6b8ecf42b1d33f280efcc07e3ac26d6
robecor <robert.corolea@cultofcoders.com>
2018-10-21 17:13:02 +0300

    Initial client setup

--- server/startup/serverListen.js
+++ server/startup/serverListen.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+
+//Define the absolute path and the view path
+const absolutePath = `${__dirname}/../../`;
+const viewPath = `${absolutePath}/client/views`;
+
+app.use(express.static(viewPath));
 
 app.get("/", function (req, res) {
-  console.log("Hello world");
-  res.send("Hello world!")
+  res.sendFile(path.join(`${viewPath}/index.html`));
 });
 
 app.listen(3000, function () {
